214f5bfc12aea1b7fcf5dbd8de99fdc53877fcb7
ZayaByghead <ZayaByghead@gmail.com>
2022-02-06 20:52:33 +0800

    changed something

--- webhook.js
+++ webhook.js
@@ -1,7 +1,7 @@
 const readline = require('readline')
 const rl = readline.createInterface({
 	input: process.stdin,
-	output: process.stdout,
+	output: process.stdout
 })
 
 const botConfig = require('./bot.config')
